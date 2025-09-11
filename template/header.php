578ca2742eb39dcb199b8c87bf071f4ec6b8d221
ferripe <jfelipe3944@gmail.com>
2025-09-10 21:25:45 -0500

    vreación carpeta img y subida del logo

--- template/header.php
+++ template/header.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- Boostrap --><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>Business Management </title>
 </head>
 <body>
+    <header>
+        <!-- Barra de navegación -->
+         
+        <nav>
+            <img src="" alt="">
+            <ul>
+                <li><a href="home.php">Inicio</a></li>
+                <li><a href="registrar.php">Registrar</a></li>
+                <li><a href="ingresar.php">Ingresar</a></li>
+            </ul>
+        </nav>
+    </header>
     
-    <nav>
-        <ul>
-            <li><a href="home.php">Inicio</a></li>
-            <li><a href="registrar.php">Registrar</a></li>
-            <li><a href="ingresar.php">Ingresar</a></li>
-        </ul>
-    </nav>
