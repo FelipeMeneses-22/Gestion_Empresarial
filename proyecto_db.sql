ad46daa7afdc45563d59e853f4bbf4058afcb0e7
Felipe Meneses <jfelipe3944@gmail.com>
2026-04-22 12:52:21 -0500

    Actualizado

--- proyecto_db.sql
+++ proyecto_db.sql
@@ -1,266 +1,114 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-04-2026 a las 01:10:57
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+CREATE DATABASE IF NOT EXISTS proyecto_db;
+USE proyecto_db;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- =========================
+-- TABLA: categorias
+-- =========================
+CREATE TABLE categorias (
+  id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+  nombre_categoria VARCHAR(30) NOT NULL,
+  descripcion VARCHAR(100)
+) ENGINE=InnoDB;
 
+-- =========================
+-- TABLA: usuarios
+-- =========================
+CREATE TABLE usuarios (
+  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  correo VARCHAR(50) NOT NULL UNIQUE,
+  contrasena VARCHAR(255) NOT NULL,
+  rol TINYINT NOT NULL
+) ENGINE=InnoDB;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- =========================
+-- TABLA: productos
+-- =========================
+CREATE TABLE productos (
+  id_producto INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(100),
+  precio_venta DECIMAL(10,2) NOT NULL,
+  stock_actual INT NOT NULL DEFAULT 0,
+  stock_minimo INT NOT NULL DEFAULT 0,
+  estado BOOLEAN NOT NULL DEFAULT 1,
+  id_categoria INT NOT NULL,
 
---
--- Base de datos: `proyecto_db`
---
+  FOREIGN KEY (id_categoria)
+    REFERENCES categorias(id_categoria)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
--- --------------------------------------------------------
+-- =========================
+-- TABLA: pedidos
+-- =========================
+CREATE TABLE pedidos (
+  id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+  id_usuario INT NOT NULL,
+  fecha DATE NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  estado VARCHAR(20) NOT NULL,
 
---
--- Estructura de tabla para la tabla `categorias`
---
+  FOREIGN KEY (id_usuario)
+    REFERENCES usuarios(id_usuario)
+    ON DELETE RESTRICT
+) ENGINE=InnoDB;
 
-CREATE TABLE `categorias` (
-  `id_categoria` int(11) NOT NULL,
-  `nombre_categoria` varchar(30) DEFAULT NULL,
-  `descripcion` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- =========================
+-- TABLA: detalle_pedido
+-- =========================
+CREATE TABLE detalle_pedido (
+  id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+  cantidad INT NOT NULL,
+  fecha_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+  precio_unitario DECIMAL(10,2) NOT NULL,
+  sub_total DECIMAL(10,2) NOT NULL,
+  id_producto INT NOT NULL,
+  id_pedido INT NOT NULL,
 
--- --------------------------------------------------------
+  FOREIGN KEY (id_producto)
+    REFERENCES productos(id_producto)
+    ON DELETE RESTRICT,
 
---
--- Estructura de tabla para la tabla `comprobante`
---
+  FOREIGN KEY (id_pedido)
+    REFERENCES pedidos(id_pedido)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
-CREATE TABLE `comprobante` (
-  `id_comprobante` int(11) NOT NULL,
-  `numero_factura` int(11) DEFAULT NULL,
-  `metodo_pago` varchar(20) DEFAULT NULL,
-  `impuestos` decimal(10,2) DEFAULT NULL,
-  `fecha_emision` date DEFAULT NULL,
-  `id_pedido` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- =========================
+-- TABLA: comprobante
+-- =========================
+CREATE TABLE comprobante (
+  id_comprobante INT AUTO_INCREMENT PRIMARY KEY,
+  numero_factura INT NOT NULL,
+  metodo_pago VARCHAR(20) NOT NULL,
+  impuestos DECIMAL(10,2) NOT NULL,
+  fecha_emision DATE NOT NULL,
+  id_pedido INT NOT NULL,
 
--- --------------------------------------------------------
+  FOREIGN KEY (id_pedido)
+    REFERENCES pedidos(id_pedido)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
---
--- Estructura de tabla para la tabla `detalle_pedido`
---
+-- =========================
+-- TABLA: movimientos_inventario
+-- =========================
+CREATE TABLE movimientos_inventario (
+  id_movimiento INT AUTO_INCREMENT PRIMARY KEY,
+  cantidad INT NOT NULL,
+  tipo VARCHAR(20) NOT NULL,
+  cantidad_mov INT NOT NULL,
+  fecha DATE NOT NULL,
+  id_producto INT NOT NULL,
+  id_usuario INT NOT NULL,
 
-CREATE TABLE `detalle_pedido` (
-  `id_detalle` int(11) NOT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `fecha_hora` datetime DEFAULT NULL,
-  `precio_unitario` decimal(10,2) DEFAULT NULL,
-  `sub_total` decimal(10,2) DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `id_pedido` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  FOREIGN KEY (id_producto)
+    REFERENCES productos(id_producto)
+    ON DELETE RESTRICT,
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `movimientos_inventario`
---
-
-CREATE TABLE `movimientos_inventario` (
-  `id_movimiento` int(11) NOT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `tipo` varchar(20) DEFAULT NULL,
-  `cantidad_mov` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `id_usuario` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedidos`
---
-
-CREATE TABLE `pedidos` (
-  `id_pedido` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `total` decimal(10,2) DEFAULT NULL,
-  `estado` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `id_producto` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `descripcion` varchar(50) DEFAULT NULL,
-  `precio_venta` decimal(10,2) DEFAULT NULL,
-  `stock_actual` int(11) DEFAULT NULL,
-  `stock_minimo` int(11) DEFAULT NULL,
-  `estado` tinyint(1) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `correo` varchar(50) DEFAULT NULL,
-  `contrasena` varchar(255) DEFAULT NULL,
-  `rol` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id_categoria`);
-
---
--- Indices de la tabla `comprobante`
---
-ALTER TABLE `comprobante`
-  ADD PRIMARY KEY (`id_comprobante`),
-  ADD KEY `id_pedido` (`id_pedido`);
-
---
--- Indices de la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  ADD PRIMARY KEY (`id_detalle`),
-  ADD KEY `id_producto` (`id_producto`),
-  ADD KEY `id_pedido` (`id_pedido`);
-
---
--- Indices de la tabla `movimientos_inventario`
---
-ALTER TABLE `movimientos_inventario`
-  ADD PRIMARY KEY (`id_movimiento`),
-  ADD KEY `id_producto` (`id_producto`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`id_pedido`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_categoria` (`id_categoria`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `comprobante`
---
-ALTER TABLE `comprobante`
-  MODIFY `id_comprobante` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `movimientos_inventario`
---
-ALTER TABLE `movimientos_inventario`
-  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `comprobante`
---
-ALTER TABLE `comprobante`
-  ADD CONSTRAINT `comprobante_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`);
-
---
--- Filtros para la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  ADD CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
-  ADD CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`);
-
---
--- Filtros para la tabla `movimientos_inventario`
---
-ALTER TABLE `movimientos_inventario`
-  ADD CONSTRAINT `movimientos_inventario_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
-  ADD CONSTRAINT `movimientos_inventario_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
-
---
--- Filtros para la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  FOREIGN KEY (id_usuario)
+    REFERENCES usuarios(id_usuario)
+    ON DELETE RESTRICT
+) ENGINE=InnoDB;
